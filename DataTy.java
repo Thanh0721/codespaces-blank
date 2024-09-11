39761bb58f4ca6b8ecfcfb25f5cf90492b1a0639
Thanh0721 <thanhn2789@nycstudents.net>
2024-09-11 00:30:33 +0000

    git push

---
+++ DataTy.java
@@ -0,0 +1,21 @@
+public class DataTy
+{
+    public static void main(String[] args) 
+    {
+
+        int myInt = 10;
+        double myDouble = 5.5;
+        boolean myBoolean = true;
+
+        String myString = "Hello!";
+
+        double result = myInt + myDouble;
+
+        myInt = myInt + 10;
+        
+        System.out.println(myString);
+        System.out.println(myInt);
+        System.out.print(result);
+
+    }
+}
